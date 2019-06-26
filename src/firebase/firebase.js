2350d36674c520d54276e02d7cb0999cc15ea10d
ringoyip0901 <ringoy@epath.org>
2019-06-26 16:27:00 -0700

    added goals

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,7 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 // Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  projectId: ""
-});
+firebase.initializeApp({});
 
 var db = firebase.firestore();
 
@@ -17,6 +13,9 @@ class FirebaseAction {
       .collection("users-goals")
       .doc("RANDOM-ID-2")
       .set({
+        newGoals: {
+          list: goals.columns["column-0"].taskIds
+        },
         needs: {
           list: goals.columns["column-1"].taskIds
         },
